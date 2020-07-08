ee8083e8de925714b0cd7302e9acbcf1a35a072f
Reid Ellis <rae@tnir.org>
2020-07-08 14:22:39 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Reid Ellis on 2020-07-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swift_ui_documentApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_documentApp.swift
+//  Shared
+//
+//  Created by Reid Ellis on 2020-07-08.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_documentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
